1c366a3d92f0393d3555bec287a86c0d02cf721a
Anwarkamal143 <anwarkamal1434@gmail.com>
2026-03-16 16:43:59 +0500

    added suspense

--- src/app/(dashboard)/(home)/credentials/new/page.tsx
+++ src/app/(dashboard)/(home)/credentials/new/page.tsx
@@ -1,10 +1,27 @@
+import { HydrateClient } from '@/components/server'
 import CredentialForm from '@/features/credentials/components/credential'
+import {
+  CredentialsError,
+  CredentialsLoading,
+} from '@/features/credentials/components/credentials'
+import { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
 
 const NewCredentialPage = () => {
   return (
     <div className='h-full p-4 md:px-10 md:py-6'>
       <div className='mx-auto flex h-full w-full max-w-6xl flex-col gap-y-8'>
-        <CredentialForm />
+        <HydrateClient>
+          <ErrorBoundary
+            fallback={<CredentialsError message='Error loading credential' />}
+          >
+            <Suspense
+              fallback={<CredentialsLoading message='Loading credential...' />}
+            >
+              <CredentialForm />
+            </Suspense>
+          </ErrorBoundary>
+        </HydrateClient>
       </div>
     </div>
   )
