0c32f5167940bdf7d8d3d81ba0b93c82cbb29b4e
Anwarkamal143 <anwarkamal1434@gmail.com>
2026-03-30 13:53:08 +0500

    updated signup page

--- src/app/(dashboard)/(home)/server/page.tsx
+++ src/app/(dashboard)/(home)/server/page.tsx
@@ -1,19 +1,19 @@
-import Dataloader from '@/components/loaders'
-import { isAccessTokenRefresing } from '@/lib'
-import { authSession } from '@/lib/auth/auth'
+// import Dataloader from '@/components/loaders'
+// import { isAccessTokenRefresing } from '@/lib'
+// import { authSession } from '@/lib/auth/auth'
 import Link from 'next/link'
 
 type Props = { searchParams: Promise<any> }
 
 async function Page({ searchParams }: Props) {
   const sparams = await searchParams
-  if (isAccessTokenRefresing(sparams)) {
-    return <Dataloader />
-  }
-  const resp = await authSession(sparams)
+  // if (isAccessTokenRefresing(sparams)) {
+  //   return <Dataloader />
+  // }
+  // const resp = await authSession(sparams)
   return (
     <div>
-      {JSON.stringify(resp, null, 2)}
+      {/* {JSON.stringify(resp, null, 2)} */}
       <Link href={'/workflows?workflwos=true'}>Workflows</Link>
     </div>
   )
